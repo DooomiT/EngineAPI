2443abfe55091bceb7ba8b53c60f31a676409d8c
Florian Bufler <inf18025@lehre.dhbw-stuttgart.de>
2020-07-22 15:09:34 +0200

    Implemented Logger skeleton

---
+++ source/Logger/_inc/ILogger.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "EngineApiDatatypes.hpp"
+
+class ILogger
+{
+    ILogger() = default;
+    virtual bool getState(LoggerState &f_logger_state) = 0;
+    virtual bool logFatal(std::string &f_log_string) = 0;
+    virtual bool logError(std::string &f_log_string) = 0;
+    virtual bool logDebug(std::string &f_log_string) = 0;
+    virtual bool logInfo(std::string &f_log_string) = 0;
+};
